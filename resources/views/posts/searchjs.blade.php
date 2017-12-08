8fb87c8822571ce3c9d8880b918dd2e85cc5984a
J. Alexander Curtis <jalexandercurtis@gmail.com>
2017-12-07 23:16:47 -0700

    2.8 Part 2: Finished Frontend Vue.js Search with Styling

--- resources/views/posts/searchjs.blade.php
+++ resources/views/posts/searchjs.blade.php
@@ -5,31 +5,30 @@
     <ais-index app-id="{{ config('scout.algolia.id') }}"
                 api-key="{{ env('ALGOLIA_SEARCH') }}"
                 index-name="posts">
-    <h1>Search for Posts</h1>
-    <ais-input placeholder="Search for a post..."></ais-input>
-    <hr />
+      <h1>Search for Posts</h1>
+      <ais-input placeholder="Search for a post..." class="form-control input-lg"></ais-input>
+      <hr />
 
-    <ais-results></ais-results>
-
-      {{-- <div class="row" style="margin-top: 20px;">
-        <div class="col-md-8">
-          <a href="{{ route('posts.show', $post->id) }}"><h3>{{ $post->title }}</h3></a>
-        </div>
-        <div class="col-md-4">
-          @if ($post->published)
-            <h4><span class="label label-success pull-right">PUBLISHED</span><h4>
-          @else
-            <h4><span class="label label-default pull-right">DRAFT</span><h4>
-          @endif
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <p>
-            {{ str_limit($post->content, 250) }}
-          </p>
-        </div>
-      </div> --}}
+      <ais-results>
+        <template scope="{result}">
+          <div class="row" style="margin-top: 20px;">
+            <div class="col-md-8">
+              <a href="/posts/@{{ result.id }}"><h3>@{{ result.title }}</h3></a>
+            </div>
+            <div class="col-md-4">
+              <h4 v-if="result.published"><span class="label label-success pull-right">PUBLISHED</span><h4>
+              <h4 v-if="!result.published"><span class="label label-default pull-right">DRAFT</span><h4>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <p>
+                @{{ result.content }}
+              </p>
+            </div>
+          </div>
+        </template>
+      </ais-results>
     </ais-index>
   </div>
 @endsection
